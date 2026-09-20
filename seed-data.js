193bc7dac0668f5515b92aa2a27b9dba45457b62
transparencia-mendonca <miguel.angelogarcia@gmail.com>
2026-09-20 19:36:21 -0300

    Add files via upload

---
+++ seed-data.js
@@ -0,0 +1,2 @@
+window.SEED_DATA={_bundleVersion:"bady-bassitt-baseline",exercicios:[],despesas:[],receitas:[],apiUpdates:{},autoCheck:{}};
+window.TCESP_PERMANENT_BASELINE={manifest:"baseline-manifest.json",version:"bady-bassitt-baseline"};
